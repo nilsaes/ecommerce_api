59414e5b69950e8b0f67e62f000e911168888a8d
nilsaes <nilsaescobar23@gmail.com>
2026-05-22 11:07:41 -0300

    feat: crear modelos de productos, categorías y variantes con control de stock

---
+++ ecommerce_api/backend/core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=120, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='variants')
+    size = models.CharField(max_length=50, blank=True, null=True)  # Ejemplo: S, M, L o Talle 40
+    color = models.CharField(max_length=50, blank=True, null=True) # Ejemplo: Rojo, Negro
+    price = models.DecimalField(max_digits=10, decimal_places=2)    # Soporta montos grandes para Guaraníes o dólares
+    stock = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return f"{self.product.name} - {self.size} / {self.color}"
