b0776e678ce05f59a72a1666fa8a2e5f8af60476
Nilsa <nilsaescobar23@gmail.com>
2026-05-27 11:37:13 -0300

    Feat: Registrados los modelos Cart y CartItem en el panel de administración de Django

--- ecommerce_api/backend/cart/admin.py
+++ ecommerce_api/backend/cart/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+class CartItemInline(admin.TabularInline):
+    """Esto permite ver y editar los productos adentro del mismo carrito"""
+    model = CartItem
+    extra = 0
+    readonly_fields = ['subtotal']
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'total_price', 'created_at', 'updated_at']
+    inlines = [CartItemInline] # Metemos los ítems adentro del panel del carrito
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cart', 'product_variant', 'quantity', 'subtotal']
