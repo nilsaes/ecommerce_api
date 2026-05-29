68d2fa718f7767592b30156a89b979f26a00d9be
Nilsa <nilsaescobar23@gmail.com>
2026-05-29 10:33:23 -0300

    Config: Instalado y configurado django-cors-headers para permitir la conexión con el Frontend

--- ecommerce_api/backend/config/settings.py
+++ ecommerce_api/backend/config/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    'corsheaders',
     'core',
     'cart',  # <-- Agregamos nuestra nueva app acá
     'users', # <-- La agregamos acá
@@ -23,6 +24,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -106,3 +108,4 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
+CORS_ALLOW_ALL_ORIGINS = True
