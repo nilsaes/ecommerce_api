95d9e3ba988dfa9f0239c75339b2a6b621046e44
Nilsa <nilsaescobar23@gmail.com>
2026-06-17 20:08:55 -0300

    fix: resolver error 500 calculando total y subtotal con SerializerMethodField

--- ecommerce_api/backend/cart/serializers.py
+++ ecommerce_api/backend/cart/serializers.py
@@ -3,7 +3,6 @@ from .models import Cart, CartItem
 from core.models import ProductVariant
 
 class CartItemSerializer(serializers.ModelSerializer):
-    # Traemos detalles de la variante para que el frontend pueda mostrar el nombre, talla y color
     product_name = serializers.CharField(source='product_variant.product.name', read_only=True)
     size = serializers.CharField(source='product_variant.size', read_only=True)
     color = serializers.CharField(source='product_variant.color', read_only=True)
@@ -14,7 +13,7 @@ class CartItemSerializer(serializers.ModelSerializer):
         fields = ['id', 'product_variant', 'product_name', 'size', 'color', 'price', 'quantity', 'subtotal']
 
     def validate(self, data):
-        # ¡Validación Avanzada de Stock! Evitamos que metan al carrito más de lo que hay disponible
+
         variant = data['product_variant']
         quantity = data['quantity']
         
@@ -26,9 +25,12 @@ class CartItemSerializer(serializers.ModelSerializer):
 
 
 class CartSerializer(serializers.ModelSerializer):
-    # Anidamos los ítems adentro del carrito para ver todo el detalle junto
+
     items = CartItemSerializer(many=True, read_only=True)
-    
+    total_price = serializers.SerializerMethodField()
     class Meta:
         model = Cart
-        fields = ['id', 'user', 'items', 'total_price', 'created_at', 'updated_at']
+        fields = ['id', 'user', 'items', 'total_price']
+        #fields = ['id', 'user', 'items', 'total_price', 'created_at', 'updated_at']
+        def get_total_price(self, obj):
+         return sum(item.quantity * item.product_variant.price for item in obj.items.all())
